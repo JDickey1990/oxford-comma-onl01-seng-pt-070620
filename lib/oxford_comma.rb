3e9510cbc84e47d622e3448ec0000c8494987740
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-12 03:08:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   if array.length==1
     array.join
   elsif array.length==2
-    array.join("and ")
+    array.join(" and ")
   elsif array.length>=3
-   array[-1].prepend " and " 
+   array[-1].prepend "and " 
    array.join(", ")
   end
 end
